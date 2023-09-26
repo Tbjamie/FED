239cbde29b8c33aa9f1d516e44a03d488a5bb4d5
Tbjamie <144114217+Tbjamie@users.noreply.github.com>
2023-09-26 15:27:12 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,16 +6,29 @@ var mainHeaderMobile = document.querySelector('header ul')
 var hamburgerCloseButton = document.querySelector('header nav > button')
 var headerTransparent = document.querySelector('header')
 var body = document.querySelector('body')
+var details = document.querySelectorAll('details');
+var hamburgerMenuBlur = document.querySelector('#home main::after')
+
+var pageChecker = window.location.href;
+var mediaQuery750 = window.matchMedia('(min-width: 750px)')
 
 // FUNCTIONS
 
 function hamburgerOpen() {
-    mainHeaderMobile.style.display = 'none';
-    hamburgerMenuOpen.style.transform = 'translateX(0)';
+    if(!mediaQuery750.matches) {
+        mainHeaderMobile.style.display = 'none';
+        hamburgerMenuOpen.style.transform = 'translateX(0)';
+        console.log("HET WERKT")
+    } else if(mediaQuery750.matches) {
+        mainHeaderMobile.style.display = 'none';
+        hamburgerMenuOpen.style.transform = 'translateX(16.5em)';
+    }
 }
 
+// console.log(hamburgerMenuBlur);
+
 function hamburgerClose() {
-    hamburgerMenuOpen.style.transform = 'translateX(30em)';
+    hamburgerMenuOpen.style.transform = 'translateX(60em)';
     setTimeout(bringMenuBack, 200);
 }
 
@@ -28,6 +41,28 @@ function reactiveHeader() {
     headerTransparent.style.backgroundColor = 'var(--headergrey)';
     headerTransparent.style.borderBottom = '2px solid var(--grey)';
 }
+
+function headerAboutRiot() {
+    if(pageChecker.includes("about-riot")) {
+        reactiveHeader();
+    } else {
+        return;
+    }
+}
+
+// details.forEach((targetDetail) => {
+//   targetDetail.addEventListener("click", () => {
+//     // Close all the details that are not targetDetail.
+//     details.forEach((detail) => {
+//       if (detail !== targetDetail) {
+//         detail.removeAttribute("open");
+//       }
+//     });
+//   });
+// });
+
+// BRON: https://stackoverflow.com/questions/16751345/automatically-close-all-the-other-details-tags-after-opening-a-specific-detai 
+
 
 // function getPosition() {
 //     return body.scrollTop;
@@ -54,8 +89,14 @@ function reactiveHeader() {
 // console.log(positionStatusCheck());
 
 
+
 // EVENTS
 
 hamburgerButton.addEventListener('click', hamburgerOpen);
 hamburgerCloseButton.addEventListener('click', hamburgerClose);
 body.onscroll = reactiveHeader;
+
+
+// EXCECUTES
+
+headerAboutRiot();
